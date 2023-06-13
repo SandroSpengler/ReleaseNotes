33ac8d54af656e33310721b4bd3d8b823ff4f6a8
sandro spengler <sandro.spengler@web.de>
2023-06-13 15:51:10 +0200

    Changed Swagger-Configuration to use / instead of http://

--- src/main/java/com/notes/ReleaseNotes/ReleaseNotesApplication.java
+++ src/main/java/com/notes/ReleaseNotes/ReleaseNotesApplication.java
@@ -4,9 +4,12 @@ import io.swagger.v3.oas.models.ExternalDocumentation;
 import io.swagger.v3.oas.models.OpenAPI;
 import io.swagger.v3.oas.models.info.Info;
 import io.swagger.v3.oas.models.info.License;
+import io.swagger.v3.oas.models.servers.Server;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.Bean;
+
+import java.util.List;
 
 @SpringBootApplication
 public class ReleaseNotesApplication {
@@ -19,6 +22,7 @@ public class ReleaseNotesApplication {
     @Bean
     public OpenAPI springShopOpenAPI() {
         return new OpenAPI()
+                .servers(List.of(new Server().url("/")))
                 .info(new Info().title("Release Note API")
                         .description("An API that provides release notes")
                         .version("v0.0.1")
